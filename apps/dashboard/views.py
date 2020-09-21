c7331605ac9fda6dd6a75e69baaac0a89c519872
Thomas Svendal <thomassvendal@gmail.com>
2020-09-21 21:38:47 +0200

    first commit

---
+++ apps/dashboard/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_screen(request):
+    return render(request, 'dashboard/blank.html')
---
+++ apps/droneManager/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DronemanagerConfig(AppConfig):
+    name = 'droneManager'
